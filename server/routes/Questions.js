25f5dcf89d88b554de923fabd74983661a5233a3
Kshitiz Gupta <128485948+kshitiz1108@users.noreply.github.com>
2023-07-18 18:23:37 +0530

    Update Questions.js

--- server/routes/Questions.js
+++ server/routes/Questions.js
@@ -9,9 +9,9 @@ import { voteQuestion } from "../controller/Questions.js";
 
 const router = express.Router();
 
-router.post("/Ask", AskQuestion);
+router.post("/Ask",auth ,  AskQuestion);
 router.get("/get" , getAllquestions)
-router.delete("/delete/:id" , deletequestion)
-router.patch("/vote/:id",  voteQuestion);
+router.delete("/delete/:id" ,auth ,  deletequestion)
+router.patch("/vote/:id", auth,  voteQuestion);
 
 export default router;
